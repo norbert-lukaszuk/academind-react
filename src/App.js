e2b05718c2a821869a3889ff54f0a3ba159e1f53
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2020-08-22 11:40:10 +0200

    outputting lists

--- src/App.js
+++ src/App.js
@@ -5,28 +5,36 @@ import { v4 as uuid } from "uuid";
 export default class App extends Component {
   state = {
     persons: [
-      { name: "Ewa", age: 23 },
-      { name: "Jan", age: 33 },
-      { name: "Borys", age: 13 },
+      { id: uuid(), name: "Ewa", age: 23 },
+      { id: uuid(), name: "Jan", age: 33 },
+      { id: uuid(), name: "Borys", age: 13 },
     ],
     newPerson: { name: "Bogdan", age: 40 },
+    listIsShowing: true,
   };
 
   buttonClickHandler = () => {
     this.setState((prevState) => {
       const updatedState = prevState.persons.map((person) => {
         if (person.name === "Jan") {
-          person.name = "Tomasz";
+          return {
+            ...person,
+            name: "Tomasz",
+            age: 45,
+          };
         }
-        // else if (person.name === "Tomasz") {
-        //   person.name = "Jan";
-        // }
+
         return person;
       });
       return {
         persons: updatedState,
       };
     });
+  };
+  showListHandler = () => {
+    const update = { ...this.state };
+    this.setState({ listIsShowing: !update.listIsShowing });
+    console.log(update);
   };
   nameCLickHandler = (newName) => {
     // persons: [
@@ -45,22 +53,41 @@ export default class App extends Component {
     // });
     this.setState((prevState) => {
       return {
-        persons: [...prevState.persons, { name: "Alex", age: 56 }],
+        persons: [...prevState.persons, { id: uuid(), name: "Alex", age: 56 }],
       };
+    });
+  };
+  inputNameHandler = (e) => {
+    const updatedValue = e.target.value;
+    this.setState((prevState) => {
+      const update = prevState.persons.map((person) => {
+        if (e.target.name === person.id) {
+          console.log(person.name);
+        }
+        return person;
+      });
     });
   };
   render() {
     console.log(this.state);
+    const PersonsList = this.state.persons.map((person) => {
+      return (
+        <Person
+          key={person.id}
+          id={person.id}
+          personName={person.name}
+          age={person.age}
+          inputNameHandler={this.inputNameHandler}
+        />
+      );
+    });
     return (
       <div>
         <h3>Hello React!</h3>
         <button onClick={this.buttonClickHandler}>Change Name</button>
         <button onClick={this.addUserHandler}>Add user</button>
-        {this.state.persons.map((person) => {
-          return (
-            <Person key={uuid()} personName={person.name} age={person.age} />
-          );
-        })}
+        <button onClick={this.showListHandler}>Show/hide list</button>
+        {this.state.listIsShowing ? PersonsList : null}
       </div>
     );
   }
